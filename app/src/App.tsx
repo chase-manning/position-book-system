f65b364b149873fc939d813cbfb607a5f55be703
Chase Manning <chase@manning.dev>
2025-05-13 22:18:18 +0100

    :rocket: add salt button to home page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,14 @@
+import { SaltProvider } from "@salt-ds/core";
+import { Button } from "@salt-ds/core";
+import "@salt-ds/theme/index.css";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <SaltProvider>
+      <div>Hello World</div>
+      <Button>Click me</Button>
+    </SaltProvider>
+  );
 };
 
 export default App;
