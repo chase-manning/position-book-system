1aef08fcf7fff543d6752dbfd0c02bcb4f06e2ab
Chase Manning <chase@manning.dev>
2025-05-13 21:22:18 +0100

    :sparkles: validation to trade events

--- api/src/main/java/com/positionbook/position_book_system/dto/TradeEventRequest.java
+++ api/src/main/java/com/positionbook/position_book_system/dto/TradeEventRequest.java
@@ -3,8 +3,14 @@ package com.positionbook.position_book_system.dto;
 import lombok.Data;
 import java.util.List;
 import com.positionbook.position_book_system.model.TradeEvent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.Valid;
 
 @Data
 public class TradeEventRequest {
+    @NotNull
+    @NotEmpty
+    @Valid
     private List<TradeEvent> events;
 } 
