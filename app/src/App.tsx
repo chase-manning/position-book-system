f4f4151d647342ff4b374d77193eb24be2cec48c
Chase Manning <chase@manning.dev>
2025-05-13 22:40:24 +0100

    :rocket: add a test banner

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,23 @@
 import { SaltProvider } from "@salt-ds/core";
-import { Button } from "@salt-ds/core";
+import { Button, Banner, BannerContent, BannerActions } from "@salt-ds/core";
+
 import "@salt-ds/theme/index.css";
 
 const App = () => {
   return (
     <SaltProvider>
       <div>Hello World</div>
-      <Button>Click me</Button>
+      <Button appearance="solid" sentiment="accented">
+        Click me
+      </Button>
+      <Banner>
+        <BannerContent>
+          <p>Hello World</p>
+        </BannerContent>
+        <BannerActions>
+          <Button>Click me</Button>
+        </BannerActions>
+      </Banner>
     </SaltProvider>
   );
 };
