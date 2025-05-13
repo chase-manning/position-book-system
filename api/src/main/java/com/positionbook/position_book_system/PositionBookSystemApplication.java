57dd4910a7aed2c2b586aa4a90221ddd16f872d3
Chase Manning <chase@manning.dev>
2025-05-13 18:46:56 +0100

    :tada: set up spring boilerplate

---
+++ api/src/main/java/com/positionbook/position_book_system/PositionBookSystemApplication.java
@@ -0,0 +1,13 @@
+package com.positionbook.position_book_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PositionBookSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PositionBookSystemApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/positionbook/position_book_system/PositionBookSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.positionbook.position_book_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionBookSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
