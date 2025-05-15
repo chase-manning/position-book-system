c69e26934bfa5bb436e6997c5453bd869a96f055
Chase Manning <chase@manning.dev>
2025-05-15 17:43:45 +0100

    :package: add jest

---
+++ app/jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.test.(ts|tsx)"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
