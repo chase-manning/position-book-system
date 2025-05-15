64a2efe3d6e2d77700e753ab6b125add9cb28adc
Chase Manning <chase@manning.dev>
2025-05-15 15:35:37 +0100

    :sparkles: create about component

---
+++ app/src/pages/About.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+const About: FC = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Learn more about the Position Book System</p>
+    </div>
+  );
+};
+
+export default About;
