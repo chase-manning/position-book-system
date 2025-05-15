6bd7af658a81e14060abd35fb3b9073badd7c5d8
Chase Manning <chase@manning.dev>
2025-05-15 16:19:19 +0100

    :bug: fix footer position issue

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import { BorderItem, BorderLayout, SaltProvider } from "@salt-ds/core";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "@salt-ds/theme/index.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
@@ -10,11 +11,18 @@ const App = () => {
     <SaltProvider>
       <Router>
         <Header />
-        <BorderLayout>
+        <BorderLayout
+          style={{
+            height: "100dvh",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
           <BorderItem
             style={{
               marginTop:
                 "calc(var(--salt-size-base) + var(--salt-spacing-200))",
+              flex: 1,
             }}
             position="center"
           >
@@ -24,15 +32,7 @@ const App = () => {
             </Routes>
           </BorderItem>
           <BorderItem position="south">
-            <div
-              style={{
-                padding: "var(--salt-spacing-200)",
-                margin: "var(--salt-spacing-200)",
-                backgroundColor: "var(--salt-color-gray-10)",
-              }}
-            >
-              <span>Footer</span>
-            </div>
+            <Footer />
           </BorderItem>
         </BorderLayout>
       </Router>
