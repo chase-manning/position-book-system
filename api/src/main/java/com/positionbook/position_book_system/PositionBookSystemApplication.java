d6cf0997248a76883b60bbd2fdc538cb99eda099
Chase Manning <chase@manning.dev>
2025-05-15 15:42:56 +0100

    :bug: fix cors issue locally

--- api/src/main/java/com/positionbook/position_book_system/PositionBookSystemApplication.java
+++ api/src/main/java/com/positionbook/position_book_system/PositionBookSystemApplication.java
@@ -2,6 +2,9 @@ package com.positionbook.position_book_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PositionBookSystemApplication {
@@ -10,4 +13,17 @@ public class PositionBookSystemApplication {
 		SpringApplication.run(PositionBookSystemApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+					.allowedOrigins("http://localhost:5173")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("*")
+					.allowCredentials(true);
+			}
+		};
+	}
 }
