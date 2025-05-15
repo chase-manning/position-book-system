aec4d3c7f6f5473fd7486671c7310322b67242ad
Chase Manning <chase@manning.dev>
2025-05-15 15:35:44 +0100

    :sparkles: create home component

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Position Book System</p>
+    </div>
+  );
+};
+
+export default Home;
